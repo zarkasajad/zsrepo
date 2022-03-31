353460d708094991410b16213c27f79ab60a6602
zarka <zarkawani09361@gmail.com>
2022-03-31 16:07:37 +0530

    changes yo be commited asian

--- Mavenjava/src/test/java/qaclickacademy/RESTAPITest.java
+++ Mavenjava/src/test/java/qaclickacademy/RESTAPITest.java
@@ -8,6 +8,8 @@ public class RESTAPITest {
 	public void postJira()
 	{
 		System.out.println("postJira");
+		System.out.println("updatinggg");
+		System.out.println("updatingggagain");
 	}
 	
 	@Test
